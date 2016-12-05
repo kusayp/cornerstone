78118b8f2719d90447ca49192672b871b2f2bd33
Joseph Tetteh <josephkusaytetteh@gmail.com>
2016-12-05 03:26:01 +0000

    make it better

--- cbbc/settings.py
+++ cbbc/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@b@g=k2!$*lqi+5%3qhl-^w_c5@4hut8r()gqjfg&52m*c9m4-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
