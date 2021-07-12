f856b2fd061132192f0055080ccd1c82abd5cc6a
joseph <josephkusaytetteh@gmail.com>
2021-07-12 11:07:29 +0000

    added README.md

--- cbbc/settings.py
+++ cbbc/settings.py
@@ -146,7 +146,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'config/db.sqlite3'),
         }
     }
 
